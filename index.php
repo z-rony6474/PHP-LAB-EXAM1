78c79dd84abcb95a78c45bac3e15ac9e2e19bd23
Zahidul Islam Roni <111661615+z-rony6474@users.noreply.github.com>
2023-06-21 17:11:36 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,62 @@
 
 <!DOCTYPE html>
 <html lang="en">
+<head>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f2f2f2;
+		}
+
+		.container {
+			width: 400px;
+			margin: 0 auto;
+			padding: 20px;
+			background-color: #ffffff;
+			border-radius: 10px;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		}
+
+		h1 {
+			color: #0c3294;
+			font-size: 24px;
+			margin-top: 0;
+		}
+
+		button {
+			background-color: #0c3294;
+			border: none;
+			padding: 12px 20px;
+			border-radius: 6px;
+			color: white;
+			margin: 10px;
+			cursor: pointer;
+			font-size: 16px;
+			transition: background-color 0.3s ease;
+		}
+		
+		button:hover {
+			background-color: #144aad;
+		}
+		
+		a {
+			text-decoration: none;
+			color: #0c3294;
+			font-size: 16px;
+		}
+
+		a:hover {
+			text-decoration: underline;
+		}
+	</style>
+</head>
 <body>	
 	
-		<center>
-
-			<h1>Welcome To PHP Lab</h1>
-		  <pre><h2>Don't have an account?  <a href="registration.php"><button style="background-color:#0c3294;border: none;padding: 15px;border-radius: 10px;color:white;">Registration </button></a></h2></pre><br>
-		   <pre><h2>Already have an account?  <a href="login.php"><button style="background-color:#0c3294;border: none;padding: 15px;border-radius: 10px;color:white;">Sign In</button></a></h2></pre>
-		</center>
+	<div class="container">
+		<h1>Welcome To PHP Class</h1>
+		<a href="registration.php"><button>Registration</button></a>
+		<p>Already have an account? <a href="login.php">Login</a></p>
+	</div>
 	
 </body>
 </html>
-
-
-
-
